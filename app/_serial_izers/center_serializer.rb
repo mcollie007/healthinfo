d0bca81a0fecd8465a773763ea16ad31c099fc02
Michael Collie-Acasio <mcollie_fhc@hotmail.com>
2015-03-22 23:30:45 -0400

    removed angular and bower components

--- app/_serial_izers/center_serializer.rb
+++
@@ -1,3 +0,0 @@
-class CenterSerializer < ActiveModel::Serializer
-	attributes :zip_code, :county_name, :full_county_name, :city
-end
--- app/serializers/center_serializer.rb
+++
@@ -1,5 +0,0 @@
-class CenterSerializer < ActiveModel::Serializer
-  attributes :id, :zip_code, :county_name, :full_county_name, :city
-
-  url :center
-end
