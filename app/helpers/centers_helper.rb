fef16d1cab9fa1dca65bd3786001b3c34af9daba
Michael Collie-Acasio <mcollie_fhc@hotmail.com>
2015-03-25 09:51:43 -0400

    search results cosmetic adjustments

--- app/helpers/centers_helper.rb
+++ app/helpers/centers_helper.rb
@@ -1,4 +1,26 @@
 module CentersHelper
 
+	def self.name_format(str)
+
+		if str.length > 40 
+			return str[0..40].gsub(/\s\w+\s*$/,'...')
+		else
+			return str
+		end
+	end
+
+	def name_format(str)
+		return CentersHelper.name_format(str)
+	end
+
+	def self.zipcode_format(str)
+		zip = str.split("-")
+		return zip[0]
+	end
+
+	def zipcode_format(str)
+		return CentersHelper.zipcode_format(str)
+	end
+
 
 end
